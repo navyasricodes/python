e943d0d7ecc3036a6e0d82886f42ad7c88ef23ee
navyasricodes <devarasettynavya2006@gmail.com>
2026-07-08 11:36:28 +0530

    Basics of py

--- Basics.py
+++ Basics.py
@@ -1,10 +1,19 @@
 print("Hi! Navya")
-sum = 2 + 4
-print(sum)
+a = 2 + 4
+b=2
+c=a-b
+d=c
+print(a)
+print(d)
+d=2**4
+print(d)
 a=4
 b=a
 print(b)
+f=d/a
+print(f)
 name = 'Navya'
 print(name)
 name ='navya'
 print((name + "\n" ) * 10)
+print(r'py:\docs\navya')
