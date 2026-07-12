08efe02e7cafa1fdfe78ea802bfbfc8ffebae19e
navyasricodes <devarasettynavya2006@gmail.com>
2026-07-12 17:17:20 +0530

    import math

---
+++ Import math functions.py
@@ -0,0 +1,12 @@
+import math
+print(math.sqrt(4))
+print(math.sqrt(62))
+print(math.pi)
+import math as m
+print(m.pi)
+print(m.sqrt(8))
+from math import sqrt,pow
+print(pow(2,20))
+from operator import add
+print(add(2,60))
+print(add(2,pi))
