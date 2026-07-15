bd7d47282ac72005b5199da311d5cce991019086
navyasricodes <devarasettynavya2006@gmail.com>
2026-07-15 19:45:30 +0530

    import

--- Import math functions.py
+++ Import math functions.py
@@ -2,10 +2,13 @@ import math
 print(math.sqrt(4))
 print(math.sqrt(62))
 print(math.pi)
+
 import math as m
 print(m.pi)
+
 print(m.sqrt(8))
 from math import sqrt,pow
 print(pow(2,20))
+
 from operator import add
 print(add(2,60))
--- argv.py
+++ argv.py
@@ -1,14 +1,7 @@
+
+
+#cube
 import sys
-x=int(sys.argv[1])
-y=int(sys.argv[2])
-z=x-y
-print(z)
-
-
-
-
-#Name
-
-import sys
-name = input(sys.argv[1])
-print(name)
+num = int(sys.argv[1])
+cube=num**3
+print("cube of the number is:",cube)
