443707ad9049c469e9b756c7668a2828993e14e1
navyasricodes <devarasettynavya2006@gmail.com>
2026-07-15 20:09:58 +0530

    if_else

--- argv.py
+++ argv.py
@@ -1,4 +1,13 @@
+import sys
+x=int(sys.argv[1])
+y=int(sys.argv[2])
+z=x-y
+print(z)
 
+#Name
+import sys
+name = input(sys.argv[1])
+print(name)
 
 #cube
 import sys
---
+++ if and if else.py
@@ -0,0 +1,7 @@
+x=90
+if x==60:
+    print("good")
+else:
+    print("Bad")
+
+   
