043fc03cf3130d9a39218ed7334e59f3632344cd
navyasricodes <devarasettynavya2006@gmail.com>
2026-07-16 17:48:10 +0530

    while

---
+++ while.py
@@ -0,0 +1,15 @@
+i=1
+while i<=5:
+  print("navya", end=" ")
+  k=1
+  while k<=4:
+    print("ECE", end=" ")
+    k=k+1
+  j=1
+  while j<=4:
+    print("sri", end=" ")
+    j=j+1
+  i=i+1
+ 
+  print()
+
