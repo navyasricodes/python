9bd2d27411ddb6a98a9d20467177a9137bf7329c
navyasricodes <devarasettynavya2006@gmail.com>
2026-07-18 20:19:17 +0530

    while loop

---
+++ while loop.py
@@ -0,0 +1,15 @@
+i = ['navya',403,8.6]
+for sri in i:
+    print(sri)
+
+
+#Range
+for i in range(10, 20):
+    print(i)
+
+
+#Range distance
+for x in range(10,50,2):
+    if x%3==0:
+        print(x)
+    
