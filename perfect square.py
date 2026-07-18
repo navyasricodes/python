9d5e166a2c3a90f6f870d2af8479855fa17aa12f
navyasricodes <devarasettynavya2006@gmail.com>
2026-07-18 20:54:55 +0530

    perfect square

---
+++ perfect square.py
@@ -0,0 +1,12 @@
+num = 1
+while num <= 100:
+    i = 1
+    while i * i <= num:
+        if i * i == num:
+            if num%2!=0:
+             print(num)
+        i = i  +1
+    num = num + 1
+
+
+
