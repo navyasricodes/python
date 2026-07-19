e709e0978b56fbeb221b8e40fadf616ca39d53e7
navyasricodes <devarasettynavya2006@gmail.com>
2026-07-19 14:44:14 +0530

    for loop

--- for loop.py
+++ for loop.py
@@ -2,7 +2,29 @@
    # print(i)
 
 
-for i in range(1,201):
-   if i%2==0:
-      if i%3!=0:
-         print(i)
+#Even numbers
+#for i in range(2, 200, 4):
+   #if i % 2 == 0:
+      #print(i)
+
+
+
+
+
+
+# Even numbers which is not divisible by 3
+#for i in range(1,201):
+   #if i%2==0:
+      #if i%3!=0:
+         #print(i)
+
+
+
+
+i = 2
+num = 20
+while i <= num:
+   if i % 2 == 0:
+      print(i)
+   i = i + 4
+num=num + 1
